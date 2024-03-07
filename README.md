be17947de4610f7b9a0c092485b3d3e4fd5498de
Fedora Laptop <support@bcgates.com>
2024-03-06 21:15:05 -0500

    Copied the contents from the files from the old version into the main.py and the index.html files. Updated the content to match the new template.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# black-history-fact-generator
-Displays a random Black History fact every ten seconds.
+# Black History Facts Generator
+Displays a random Black History Fact every 10 seconds.
+
+## About The Project
+
+## Website
---
+++ main.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+Title: Black History Facts Generator
+Creator: Brittany Gates (https://github.com/brittbot-bgates) | (https://bcgates.com)
+About: Displays a random Black History Fact every 10 seconds.
+"""
+
+from flask import Flask, render_template
+import random
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET", "POST"])
+def main():
+    """
+    Opens and reads the files/bhfg_facts.txt file, loops over the entire file to read each line, collects all the 
+    lines as a list, and then randomly chooses a fact to display on index.html.
+
+    :return: Display a random Black History fact to the home.html page.
+    """
+
+    with open("static/files/bhfg_facts.txt", "r") as facts:
+        fact_list = facts.readlines()
+    return render_template('index.html', black_history_fact=random.choice(fact_list))
+
+
+if __name__ == "__main__":
+    main()
